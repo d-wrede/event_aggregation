27e49a6e5f4f304b763cc13ae259ec9730e4c52d
LevinOX <daniel.wrede@posteo.de>
2023-03-24 00:26:33 +0100

    improve date recognition

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import json
-from src.extract_timestamp import extract_timestamp
+from src.extract_timestamp import extract_timestamp, filter_string
 
 json_filename = 'telegram_messages.json'
 
@@ -19,15 +19,29 @@ def main():
             message.setdefault('timestamps', {})
             message['timestamps']['startstamp'] = timestamps[0]
             message['timestamps']['endstamp'] = timestamps[1]
+            message['timestamps']['comment'] = timestamps[2]
 
     # save messages with timestamps
     with open(json_filename, 'w', encoding='utf-8') as f:
         json.dump(messages, f, indent=4, ensure_ascii=False)
 
+    with open('file.txt', 'w', encoding='utf-8') as f:
+        for message in messages:
+            if 'message' in message:
+                f.write(filter_string(message['message']) + '\n')
+                f.write('-'*50 + '\n')
+                f.write('startstamp: ' +
+                        str(message['timestamps']['startstamp']) + '\n')
+                f.write('endstamp: ' +
+                        str(message['timestamps']['endstamp']) + '\n')
+                f.write(str(message['timestamps']['comment']) + '\n\n')
+
     # further terms to be extracted:
-    # - sender/author
+    # - sender/author spaCy?
     # - place
     # - category
+    # - URL
+
 
 
 if __name__ == "__main__":
