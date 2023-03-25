13f766074166c8984eba2c5b170542d10030b793
LevinOX <daniel.wrede@posteo.de>
2023-03-25 18:15:13 +0100

    sort events by date

--- main.py
+++ main.py
@@ -9,18 +9,26 @@ def main():
     with open(json_filename, 'r', encoding='utf-8') as f:
         messages = json.load(f)
 
+    count_dates = 0
     # timestamp extraction
     for message in messages:
         if 'message' in message:
             timestamps = extract_timestamp(message['message'])
-            print(message['message'][0:200])
-            print(timestamps, '\n')
+            # print(message['message'][0:200])
+            # print(timestamps, '\n')
             # add timestamps to message
             message.setdefault('timestamps', {})
             message['timestamps']['startstamp'] = timestamps[0]
             message['timestamps']['endstamp'] = timestamps[1]
             message['timestamps']['comment'] = timestamps[2]
+            if timestamps[0] is not None:
+                count_dates += 1
+    
+    # sort messages by timestamp
+    messages.sort(key=lambda x: str(x.get('timestamps', {}).get('startstamp', '')))
 
+
+    print(f'found {count_dates} dates in {len(messages)} messages')
     # save messages with timestamps
     with open(json_filename, 'w', encoding='utf-8') as f:
         json.dump(messages, f, indent=4, ensure_ascii=False)
@@ -28,13 +36,14 @@ def main():
     with open('file.txt', 'w', encoding='utf-8') as f:
         for message in messages:
             if 'message' in message:
-                f.write(filter_string(message['message']) + '\n')
-                f.write('-'*50 + '\n')
                 f.write('startstamp: ' +
                         str(message['timestamps']['startstamp']) + '\n')
                 f.write('endstamp: ' +
                         str(message['timestamps']['endstamp']) + '\n')
-                f.write(str(message['timestamps']['comment']) + '\n\n')
+                if message['timestamps']['comment'] != '':
+                    f.write(str(message['timestamps']['comment']) + '\n')
+                f.write('-'*50 + '\n')
+                f.write(filter_string(message['message']) + '\n\n')
 
     # further terms to be extracted:
     # - sender/author spaCy?
