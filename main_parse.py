0026fccd1f2da69f057d80e7e8df8e9d03efb985
LevinOX <daniel.wrede@posteo.de>
2023-03-28 07:45:11 +0200

    add alternative approach with dateparser library

--- main_parse.py
+++ main_parse.py
@@ -1,5 +1,6 @@
 import json
 from src.extract_timestamp import extract_timestamp, filter_string
+import dateparser.search
 
 json_filename = 'telegram_messages.json'
 
@@ -9,44 +10,54 @@ def main():
     with open(json_filename, 'r', encoding='utf-8') as f:
         messages = json.load(f)
 
+    # Custom settings for dateparser
+    settings = {
+        # Order to prioritize when parsing ambiguous dates (Day, Month, Year)
+        'DATE_ORDER': 'DMY',
+        'PREFER_DATES_FROM': 'future',  # Prefer dates from the future
+        'STRICT_PARSING': False,  # Allow for approximate parsing
+        'NORMALIZE': True,  # Normalize whitespace and remove extra spaces within the date string
+        'RETURN_AS_TIMEZONE_AWARE': False,  # Return timezone-aware datetime objects
+    }
+
     count_dates = 0
     count_messages = 0
     # timestamp extraction
-    for message in messages:
+    for message in messages[:]:
         if 'message' in message and message['message'] != '':# and 'approved' not in message['timestamps']['comment']:
             count_messages += 1
-            timestamps = extract_timestamp(message['message'])
-            # print(message['message'][0:200])
+            timestamps = dateparser.search.search_dates(message['message'], languages=['de'], settings=settings)
+            
+            #extract_timestamp(message['message'])
+            print(message['message'])
             print("timestamps: ", timestamps, '\n')
             # add timestamps to message dict
             message.setdefault('timestamps', {})
-            message['timestamps']['startstamp'] = timestamps[0]
-            message['timestamps']['endstamp'] = timestamps[1]
-            message['timestamps']['comment'] += timestamps[2]
-            if timestamps[0] is not None:
+            message['timestamps'] = timestamps
+            
+            if timestamps is not None:
                 count_dates += 1
 
         # if count_messages == 5:
         #     break
-
+    
     # sort messages by timestamp
     messages.sort(key=lambda x: str(
-        x.get('timestamps', {}).get('startstamp', float('inf'))))
+        x.get('timestamps', float('inf'))))
 
     print(f'found {count_dates} dates in {len(messages)} messages')
     # save messages with timestamps
-    with open(json_filename, 'w', encoding='utf-8') as f:
-        json.dump(messages, f, indent=4, ensure_ascii=False)
+    # with open(json_filename, 'w', encoding='utf-8') as f:
+    #     json.dump(messages, f, indent=4, ensure_ascii=False)
 
     with open('file.txt', 'w', encoding='utf-8') as f:
         for message in messages:
             if 'message' in message and message['message'] != '':
-                f.write('startstamp: ' +
-                        str(message['timestamps']['startstamp']) + '\n')
-                f.write('endstamp: ' +
-                        str(message['timestamps']['endstamp']) + '\n')
-                if message['timestamps']['comment'] != '':
-                    f.write(str(message['timestamps']['comment']) + '\n')
+                f.write(str(message['timestamps']) + '\n')
+                
+                # write comment if available
+                # if message['timestamps']['comment'] is not None:
+                #     f.write(str(message['timestamps']['comment']) + '\n')
                 f.write('-'*50 + '\n')
                 f.write(filter_string(message['message']) + '\n\n')
 
